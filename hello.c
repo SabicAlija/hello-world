e6490a557bfc46ec91d66c592194d0571e82c15b
Alija Sabic <sabic.alija@gmail.com>
2017-09-19 02:11:27 +0200

    Initial commit: create hello.c and README

---
+++ hello.c
@@ -0,0 +1,19 @@
+/*
+ =============================================================================================
+ Name        : hello.c
+ Author      : Alija Sabic, sabic.alija@gmail.com
+ Version     : 1.0
+ Copyright   : This program is free software; you can redistribute it and/or
+               modify it under the terms of the GNU General Public License as
+               published by the Free Software Foundation, see http://www.gnu.org/licenses/.
+ Description : Hello world program.
+ =============================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello, World!\n");
+	return EXIT_SUCCESS;
+}
